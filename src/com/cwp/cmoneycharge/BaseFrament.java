a89ceed19a9fe5ceec1eda132590d952a88fa6dc
Lenovo <Lenovo@xiaoshuang>
2018-03-19 19:47:45 +0800

    init

---
+++ src/com/cwp/cmoneycharge/BaseFrament.java
@@ -0,0 +1,15 @@
+package com.cwp.cmoneycharge;
+
+import android.support.v4.app.Fragment;
+
+
+public  abstract class BaseFrament extends Fragment {
+
+    public abstract void filngtonext();
+
+    public abstract void filngtonpre();
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+}
